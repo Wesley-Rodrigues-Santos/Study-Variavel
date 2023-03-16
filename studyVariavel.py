d61825d3ceaf0f2e01168c2b52df9d7f7dc9abea
Wesley Rodrigues Santos <rodrigueswesley357@gmail.com>
2023-03-16 03:43:52 -0300

    commit inicial

---
+++ studyVariavel.py
@@ -0,0 +1,12 @@
+x = 10
+nome = 'João'
+lista = (1, 2, 3)
+
+print(x)
+print(nome)
+print(lista)
+
+print("Meu nome é " + nome + "e eu tenho " + str(x) + " anos.")
+nome = "João"
+idade = 25
+print("O nome é " + nome + " e a idade é " + str(idade) + ".")
